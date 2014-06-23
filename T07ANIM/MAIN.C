15080f14756b984a8053de512098d897e6859ae8
unknown <SPR06@R111-27.(none)>
2014-06-23 20:23:25 +0400

    New name for project TO7 (T07)

---
+++ T07ANIM/MAIN.C
@@ -0,0 +1,208 @@
+/* FILENAME: MAIN.C
+ * PROGRAMMER: AP6
+ * PURPOSE: Animation startup module
+ * LAST UPDATE: 07.06.2014
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include "units.h"
+
+#define WND_CLASS_NAME "My Window Class Name"
+
+/* Данные для обработки мыши */
+static INT
+  AP6_MouseGlobalX, AP6_MouseGlobalY, /* Текущие координаты */
+  AP6_MouseXOld, AP6_MouseYOld,       /* Сохраненные от прошлого кадра координаты */
+  AP6_MouseGlobalWheel;               /* Состояние колеса мыши */
+
+/* Ссылки вперед */
+LRESULT CALLBACK MainWindowFunc( HWND hWnd, UINT Msg,
+                                 WPARAM wParam, LPARAM lParam );
+
+
+/* Функция обработки захваченных мышинных сообщений.
+ * АРГУМЕНТЫ:
+ *   - код захвата:
+ *       INT Code;
+ *   - параметр сообшения ('word parameter') -
+ *     код сообщения от мыши:
+ *       WPARAM wParam;
+ *   - параметр сообшения ('long parameter') -
+ *     (MSLLHOOKSTRUCT *) для мышинных сообщений;
+ *       LPARAM lParam;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (LRESULT) - в зависимости от сообщения.
+ */
+static LRESULT CALLBACK AP6_MouseHook( INT Code, WPARAM wParam, LPARAM lParam )
+{
+  MSLLHOOKSTRUCT *hs = (MSLLHOOKSTRUCT *)lParam;
+
+  switch (wParam)
+  {
+  case WM_MOUSEMOVE:
+    AP6_MouseGlobalX = hs->pt.x;
+    AP6_MouseGlobalY = hs->pt.y;
+    break;
+  case WM_MOUSEWHEEL:
+    AP6_MouseGlobalWheel = (SHORT)HIWORD(hs->mouseData);
+    break;
+  }
+  return 0;
+} /* End of 'AP6_MouseHook' function */
+
+
+
+/* Главная функция программы.
+ * АРГУМЕНТЫ:
+ *   - дескриптор экземпляра приложения:
+ *       HINSTANCE hInstance;
+ *   - дескриптор предыдущего экземпляра приложения
+ *     (не используется и должно быть NULL):
+ *       HINSTANCE hPrevInstance;
+ *   - командная строка:
+ *       CHAR *CmdLine;
+ *   - флаг показа окна (см. SW_SHOWNORMAL, SW_SHOWMINIMIZED, SW_***):
+ *       INT ShowCmd;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (INT) код возврата в операционную систему.
+ */
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT ShowCmd )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+  
+  /* Регистрация - создание собственного класса окна */
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.cbClsExtra = 0; /* Дополнительное количество байт для класса */
+  wc.cbWndExtra = 0; /* Дополнительное количество байт для окна */
+  wc.hbrBackground = (HBRUSH)COLOR_WINDOW; /* Фоновый цвет - выбранный в системе */
+  wc.hCursor = LoadCursor(NULL, IDC_HAND);
+  wc.hIcon = LoadIcon(NULL, IDI_ERROR);
+  wc.lpszMenuName = NULL;
+  wc.hInstance = hInstance;
+  wc.lpfnWndProc = MainWindowFunc;
+  wc.lpszClassName = WND_CLASS_NAME;
+
+  /* Регистрируем класс */
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "Error", MB_ICONERROR | MB_OK);
+    return 0;
+  }
+
+  /* Создание окна */
+  hWnd = CreateWindow(WND_CLASS_NAME, "First Window Sample",
+    WS_OVERLAPPEDWINDOW,
+    1920, 0, //CW_USEDEFAULT, CW_USEDEFAULT, /* Позиция окна (x, y) - по умолчанию */
+    1280, 1000, //CW_USEDEFAULT, CW_USEDEFAULT, /* Размеры окна (w, h) - по умолчанию */
+    NULL,                         /* Дескриптор родительского окна */
+    NULL,                         /* Дескриптор загруженного меню */
+    hInstance,                    /* Дескриптор приложения */
+    NULL);                        /* Указатель на дополнительные параметры */
+
+  ShowWindow(hWnd, ShowCmd);
+  UpdateWindow(hWnd);
+
+  /*** Добавление объектов ***/
+  AP6_AnimAddUnit(AP6_GObjUnitCreate());
+  AP6_AnimAddUnit(AP6_InfoUnitCreate());
+
+  /* Запуск цикла обработки сообщений */
+  while (GetMessage(&msg, NULL, 0, 0))
+  {
+    TranslateMessage(&msg);
+    /* Передача сообщений в функцию окна */
+    DispatchMessage(&msg);
+  }
+
+  while (TRUE)
+    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+    {
+      if (msg.message == WM_QUIT)
+        break;
+      TranslateMessage(&msg);
+      /* Передача сообщений в функцию окна */
+      DispatchMessage(&msg);
+    }
+    else
+      SendMessage(hWnd, WM_TIMER, 0, 0);
+
+  return msg.wParam;
+} /* End of 'WinMain' function */
+
+/* Функция обработки сообщения окна.
+ * АРГУМЕНТЫ:
+ *   - дескриптор окна:
+ *       HWND hWnd;
+ *   - номер сообщения:
+ *       UINT Msg;
+ *   - параметр сообшения ('word parameter'):
+ *       WPARAM wParam;
+ *   - параметр сообшения ('long parameter'):
+ *       LPARAM lParam;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (LRESULT) - в зависимости от сообщения.
+ */
+LRESULT CALLBACK MainWindowFunc( HWND hWnd, UINT Msg,
+                                 WPARAM wParam, LPARAM lParam )
+{
+  HDC hDC;
+  PAINTSTRUCT ps;
+  MINMAXINFO *minmax;
+  static BOOL pause;
+
+  switch (Msg)
+  {
+  case WM_GETMINMAXINFO:
+    minmax = (MINMAXINFO *)lParam;
+    minmax->ptMaxTrackSize.y = GetSystemMetrics(SM_CYMAXTRACK) +
+      GetSystemMetrics(SM_CYCAPTION) + GetSystemMetrics(SM_CYBORDER) * 2;
+    return 0;
+  case WM_CREATE:
+    SetTimer(hWnd, 30, 1, NULL);
+
+    if (!AP6_AnimInit(hWnd))
+      return -1;
+    return 0;
+  case WM_SIZE:
+    AP6_AnimResize(LOWORD(lParam), HIWORD(lParam));
+  case WM_TIMER:
+    AP6_AnimRender();
+    AP6_AnimCopyFrame();
+    return 0;
+  case WM_CHAR:
+    switch ((CHAR)wParam)
+    {
+    case 27:
+      DestroyWindow(hWnd);
+      return 0;
+    case 'f':
+      AP6_AnimFlipFullScreen();
+      return 0;
+    case 'p':
+      AP6_AnimSetPause(pause = !pause);
+      return 0;
+    }
+    return 0;
+  case WM_ERASEBKGND:
+    return 1;
+  case WM_PAINT:
+    hDC = BeginPaint(hWnd, &ps);
+    EndPaint(hWnd, &ps);
+    AP6_AnimCopyFrame();
+    return 0;
+  case WM_DESTROY:
+    AP6_AnimClose();
+    PostQuitMessage(0);
+    KillTimer(hWnd, 30);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+} /* End of 'MyWindowFunc' function */
+
+/* END OF 'MAIN.C' FILE */
---
+++ T07ANIM/UNITGOBG.C
@@ -0,0 +1,135 @@
+/* FILENAME: UNITINFO.C
+ * PROGRAMMER: AP6
+ * PURPOSE: Animation unit samples handle module.
+ * LAST UPDATE: 13.06.2014
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "render.h"
+#include "anim.h"
+
+/* Структура описания объекта анимации */
+typedef struct tagap6UNIT_COW
+{
+  AP6_UNIT_BASE_FIELDS; /* Включение базовых полей */
+  DBL ShiftX, ShiftY;
+  INT Type;
+  ap6GOBJ Cow;
+} ap6UNIT_COW;
+
+/* Функция инициализации информационного объекта анимации.
+ * АРГУМЕНТЫ:
+ *   - указатель на "себя" - сам объект анимации:
+ *       ap6UNIT_INFO *Unit;
+ *   - указатель на контекст анимации:
+ *       ap6ANIM *Ani;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: Нет.
+ */
+static VOID GObjUnitInit( ap6UNIT_COW *Unit, ap6ANIM *Ani )
+{
+  AP6_RndGObjLoad(&Unit->Cow, "cow.object");
+} /* End of 'GObjUnitInit' function */
+
+/* Функция деинициализации информационного объекта анимации.
+ * АРГУМЕНТЫ:
+ *   - указатель на "себя" - сам объект анимации:
+ *       ap6UNIT_INFO *Unit;
+ *   - указатель на контекст анимации:
+ *       ap6ANIM *Ani;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: Нет.
+ */
+static VOID GObjUnitClose( ap6UNIT_COW *Unit, ap6ANIM *Ani )
+{
+  AP6_RndGObjFree(&Unit->Cow);
+} /* End of 'GObjUnitClose' function */
+
+/* Функция обновления межкадровых параметров информационного
+ * объекта анимации.
+ * АРГУМЕНТЫ:
+ *   - указатель на "себя" - сам объект анимации:
+ *       ap6UNIT_INFO *Unit;
+ *   - указатель на контекст анимации:
+ *       ap6ANIM *Ani;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: Нет.
+ */
+static VOID GObjUnitResponse( ap6UNIT_COW *Unit, ap6ANIM *Ani )
+{
+  if (Ani->Keys[VK_LMENU] && Ani->KeysClick[VK_RETURN])
+    AP6_AnimFlipFullScreen();
+  if (Ani->KeysClick['P'])
+    AP6_AnimSetPause(!Ani->IsPause);
+  if (Ani->Keys[VK_ESCAPE])
+    DestroyWindow(Ani->hWnd);
+} /* End of 'GObjUnitResponse' function */
+
+/* Функция построения информационного объекта анимации.
+ * АРГУМЕНТЫ:
+ *   - указатель на "себя" - сам объект анимации:
+ *       ap6UNIT *Unit;
+ *   - указатель на контекст анимации:
+ *       ap6UNIT_INFO *Ani;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ: Нет.
+ */
+static VOID GObjUnitRender( ap6UNIT_COW *Unit, ap6ANIM *Ani )
+{
+  static DBL x, y, z;
+  MATR WVP;
+
+  x += Ani->JX;
+  y += Ani->JY;
+  z += Ani->JZ;
+
+  Ani->MatrWorld = MatrMulMatr(MatrTranslate(x, y, 2 * z), MatrRotateY(y));;
+  Ani->MatrView = MatrViewLookAt(VecSet(15, 15, 15), VecSet(0, 0, 0), VecSet(0, 1, 0));
+  Ani->MatrProjection1 = MatrProjection(-Ani->Wp / 2, Ani->Wp / 2,
+                                        -Ani->Hp / 2, Ani->Hp / 2,
+                                        Ani->ProjDist, 1000);    
+
+  
+  WVP = MatrMulMatr(Ani->MatrWorld,
+    MatrMulMatr(Ani->MatrView, Ani->MatrProjection1));
+
+  
+  glLoadMatrixd(WVP.A[0]);
+/*  
+ glBegin(GL_LINES);
+    glColor3d(1, 0.5, 0.5);
+    glVertex3d(-3, 0, 0);
+    glVertex4d(1, 0, 0, 0);
+    glColor3d(0.5, 1, 0.5);
+    glVertex3d(0, -3, 0);
+    glVertex4d(0, 1, 0, 0);
+    glColor3d(0.5, 0.5, 1);
+    glVertex3d(0, 0, -3);
+    glVertex4d(0, 0, 1, 0);
+  glEnd();
+*/  
+
+  AP6_RndGObjDraw(&Unit->Cow, Ani->hDC);
+} /* End of 'GObjUnitRender' function */
+
+/* Функция создания информационного объекта анимации.
+ * АРГУМЕНТЫ: Нет.
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (ap6UNIT_INFO *) указатель на созданный объект анимации.
+ */
+ap6UNIT * AP6_GObjUnitCreate( VOID )
+{
+  ap6UNIT_COW *Unit;
+
+  if ((Unit = (ap6UNIT_COW *)AP6_AnimUnitCreate(sizeof(ap6UNIT_COW))) == NULL)
+    return NULL;
+
+    /* заполняем поля по-умолчанию */
+  Unit->Init = (VOID *)GObjUnitInit;
+  Unit->Close = (VOID *)GObjUnitClose;
+  Unit->Render = (VOID *)GObjUnitRender;
+  Unit->Response = (VOID *)GObjUnitResponse;
+  return (ap6UNIT *)Unit;
+} /* End of 'AP6_GObjUnitCreate' function */
+
+/* END OF 'UNITINFO.C' FILE */
